29633a0a049d6586551d49043085e78673830934
Felix Lai <felix.lai@hotmail.com>
2022-11-13 11:28:57 -0500

    Update GameBoard.tsx

---
+++ src/components/GameBoard.tsx
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+import { c4Columns, c4Rows } from "./constants/index";
+import GameRow from "./GameRow";
+import { Board } from "./interfaces/Board";
+import { Row } from "./interfaces/Row";
+import { Column } from "./interfaces/Column";
