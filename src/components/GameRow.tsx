efd9122c5be001b7d6c4a8a9f42d7d2cb8acb71f
Felix Lai <felix.lai@hotmail.com>
2022-11-13 11:28:13 -0500

    Update GameRow.tsx

--- src/components/GameRow.tsx
+++ src/components/GameRow.tsx
@@ -7,3 +7,19 @@ interface Props {
     row: Row;
     updateBoard: (columnIndex: number) => void;
   }
+
+  const GameRow: React.FunctionComponent<Props> = ({
+    row,
+    updateBoard
+  }: Props): JSX.Element => {
+    return (
+      <tr>
+        {row.columns.map(
+          (column: Column, i: number): JSX.Element => (
+            <Tile key={i} column={column} columnIndex={i} updateBoard={updateBoard}/>
+          )
+        )}
+      </tr>
+    );
+  };
+  export default GameRow;
