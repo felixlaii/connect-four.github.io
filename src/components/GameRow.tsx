8fdeae13118b9eb438934154113abb6da52d042e
Felix Lai <felix.lai@hotmail.com>
2022-11-13 11:28:05 -0500

    Update GameRow.tsx

--- src/components/GameRow.tsx
+++ src/components/GameRow.tsx
@@ -2,3 +2,8 @@ import React from "react";
 import { Row } from "./interfaces/Row";
 import { Column } from "./interfaces/Column";
 import Tile from "./Tile";
+
+interface Props {
+    row: Row;
+    updateBoard: (columnIndex: number) => void;
+  }
