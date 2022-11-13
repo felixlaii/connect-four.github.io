cab8699baf58e75b70b7e8302a93ba55984f401c
Felix Lai <felix.lai@hotmail.com>
2022-11-13 11:30:36 -0500

    Update GameBoard.tsx

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -13,6 +13,36 @@ const GameBoard: React.FunctionComponent = (): JSX.Element => {
       };
       const [board, setBoard] = useState<Board>(initialBoard);
       const [currPlayer, setCurrPlayer] = useState<number>(1);
+
+      const updateBoard = (columnIndex: number): void => {
+        let boardCopy: Board = board;
+        let rowIndex: number = 0;
+        let areColumnsFull = true;
+        for (let r: number = 5; r >= 0; r--) {
+          let columnPlayer = boardCopy.rows[r].columns[columnIndex].player;
+          if (!columnPlayer) {
+            boardCopy.rows[r].columns[columnIndex].player = currPlayer;
+            rowIndex = r;
+            areColumnsFull = false;
+            break;
+          }
+        }
+        if (!areColumnsFull) {
+          setBoard(boardCopy);
+          setCurrPlayer(currPlayer === 1 ? 2 : 1);
+        }
+        if (winCheck(rowIndex, columnIndex)) {
+          setBoard(initialBoard);
+          alert("player " + currPlayer + " wins");
+          setCurrPlayer(1);
+        } else {
+          if (drawCheck()) {
+            setBoard(initialBoard);
+            alert("Draw");
+            setCurrPlayer(1);
+          }
+        }
+      };
 }
 
 export default GameBoard
